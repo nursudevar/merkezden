18b14173d67cdef001a48a365cf5e2e02d9586f5
Nur Sude Var <nursudevar@gmail.com>
2026-05-27 00:53:19 +0300

    Gif Test

--- web/src/app/login/LoginClient.tsx
+++ web/src/app/login/LoginClient.tsx
@@ -56,7 +56,7 @@ function LoginPageContent() {
             <div className="auth-visual">
                 <video
                   className="auth-visual-video"
-                  src="/gifs/meko-giris.mp4"
+                  src="/gifs/meko_giris.webm"
                   autoPlay
                   muted
                   loop
