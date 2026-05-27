efc4bf421958f134fb34381f7c8495f66d4830ca
Nur Sude Var <nursudevar@gmail.com>
2026-05-27 11:28:48 +0300

    Test

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1212,7 +1212,7 @@ export default function Home() {
           <div className="pet-filter-row">
             <div className="pet-filter-media">
               <video
-                src="/gifs/meko-pet.gif"
+                src="/gifs/meko-pet.webm"
                 autoPlay
                 loop
                 muted
